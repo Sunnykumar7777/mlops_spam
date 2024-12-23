b3073756b75d9e33380e3633730f0625611aa029
tbidev <ccesstbilq@gmail.com>
2024-12-23 16:03:07 +0530

    Updated train.py

--- train.py
+++ train.py
@@ -4,25 +4,35 @@ import yaml
 from dvclive import Live
 import re
 
-def read_accuracy(file_path=r'.\models\predictions.txt'):
+
+def accurracy_score(file_path=r'.\models\predictions.txt'):
     try:
         with open(file_path, 'r') as f:
             content = f.read()
-            match = re.search(r'Model Accuracy RF: (\d+\.\d+)', content)
-            if match:
-                return float(match.group(1))
-            else:
-                raise ValueError("Not Found")
+            default_match = re.search(r'Model Accuracy RF: (\d+\.\d+)', content)
+            gb_match = re.search(r'Model Accuracy GB: (\d+\.\d+)', content)
+            
+            accuracies = {}
+            if default_match:
+                accuracies['default'] = float(default_match.group(1))
+                
+            if gb_match:
+                accuracies['gb'] = float(gb_match.group(1))
+                
+            return accuracies
+    except FileNotFoundError:
+        print(f"Error: {file_path} not found")
     except Exception as e:
-        print(f"Error reading accuracy: {str(e)}")
-        sys.exit(1)
+        print(f"Error reading accuracies: {str(e)}")
+
 
 with Live(save_dvc_exp=True) as live:
     trains_params = yaml.safe_load(open('params.yaml'))['train']
     epochs = trains_params['epochs']
     live.log_param("epochs", epochs)
-    accuracy = read_accuracy()
+    scores = accurracy_score()
     for epoch in range(epochs):
-        live.log_metric("train/accuracy", accuracy)
+        live.log_metric("train/default_accuracy", scores.get('default'))
+        live.log_metric("train/gb_accuracy", scores.get('gb', 0))
         live.log_metric("train/loss", epochs - epoch - random.random())
         live.next_step()
