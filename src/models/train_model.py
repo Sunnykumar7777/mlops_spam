cde6c710555ada05c144c43441d2df2442c0354f
tbidev <ccesstbilq@gmail.com>
2024-12-23 13:21:05 +0530

    Added new model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 import logging
 from pathlib import Path
 import joblib
@@ -21,17 +21,32 @@ def load_features(features_filepath):
     return pd.read_csv(features_filepath)
 
 
-def train_model(X_train, y_train, n_estimators, random_state):
+def train_model_rf(X_train, y_train, n_estimators, random_state, max_depth):
     logger = logging.getLogger(__name__)
     logger.info('Training Random Forest model')
     
     rf_classifier = RandomForestClassifier(
         n_estimators=n_estimators, 
-        random_state=random_state
+        random_state=random_state,
+        max_depth=max_depth
     )
     
     rf_classifier.fit(X_train, y_train)
     return rf_classifier
+
+
+def train_model_gb(X_train, y_train, n_estimators, random_state, max_depth):
+    logger = logging.getLogger(__name__)
+    logger.info('Training GradientBoosting model')
+    
+    gb_classifier = GradientBoostingClassifier(
+        n_estimators=n_estimators, 
+        random_state=random_state,
+        max_depth=max_depth
+    )
+    
+    gb_classifier.fit(X_train, y_train)
+    return gb_classifier
 
 
 def evaluate_model(model, X_test, y_test):
@@ -49,7 +64,8 @@ def evaluate_model(model, X_test, y_test):
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.argument('output_filepath2', type=click.Path())
+def main(input_filepath, output_filepath, output_filepath2):
     logger = logging.getLogger(__name__)
     
     params_path = Path(__file__).resolve().parents[2] / 'params.yaml'
@@ -73,20 +89,33 @@ def main(input_filepath, output_filepath):
     logger.info(f'Saving test data to {test_data_path}')
     test_data.to_csv(test_data_path, index=False)
     
-    model = train_model(
-        X_train, 
+    # model = train_model_rf(
+    #     X_train, 
+    #     y_train, 
+    #     n_estimators=params['model']['random_forest']['n_estimators'],
+    #     random_state=params['model']['random_forest']['random_state'],
+    #     max_depth=params['model']['random_forest']['max_depth']
+    # )
+
+    model_2 = train_model_gb(
+        X_train,
         y_train, 
-        n_estimators=params['model']['random_forest']['n_estimators'],
-        random_state=params['model']['random_forest']['random_state']
+        n_estimators=params['model']['gradient_boosting']['n_estimators'], 
+        random_state=params['model']['gradient_boosting']['random_state'], 
+        max_depth=params['model']['gradient_boosting']['max_depth']
     )
     
     # Evaluate model
-    accuracy = evaluate_model(model, X_test, y_test)
+    # accuracy = evaluate_model(model, X_test, y_test)
     
     # Save the model
-    logger.info(f'Saving model to {output_filepath}')
-    Path(output_filepath).parent.mkdir(parents=True, exist_ok=True)
-    joblib.dump(model, output_filepath)
+    # logger.info(f'Saving model to {output_filepath}')
+    # Path(output_filepath).parent.mkdir(parents=True, exist_ok=True)
+    # joblib.dump(model, output_filepath)
+
+    logger.info(f'Saving model to {output_filepath2}')
+    Path(output_filepath2).parent.mkdir(parents=True, exist_ok=True)
+    joblib.dump(model_2, output_filepath2)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
