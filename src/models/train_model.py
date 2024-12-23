2c0ba71d396b9b489034eb567a60c80e6f0abea4
tbidev <ccesstbilq@gmail.com>
2024-12-23 18:33:54 +0530

    updated train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -89,33 +89,33 @@ def main(input_filepath, output_filepath, output_filepath2):
     logger.info(f'Saving test data to {test_data_path}')
     test_data.to_csv(test_data_path, index=False)
     
-    # model = train_model_rf(
-    #     X_train, 
-    #     y_train, 
-    #     n_estimators=params['model']['random_forest']['n_estimators'],
-    #     random_state=params['model']['random_forest']['random_state'],
-    #     max_depth=params['model']['random_forest']['max_depth']
-    # )
-
-    model_2 = train_model_gb(
-        X_train,
+    model = train_model_rf(
+        X_train, 
         y_train, 
-        n_estimators=params['model']['gradient_boosting']['n_estimators'], 
-        random_state=params['model']['gradient_boosting']['random_state'], 
-        max_depth=params['model']['gradient_boosting']['max_depth']
+        n_estimators=params['model']['random_forest']['n_estimators'],
+        random_state=params['model']['random_forest']['random_state'],
+        max_depth=params['model']['random_forest']['max_depth']
     )
+
+    # model_2 = train_model_gb(
+    #     X_train,
+    #     y_train, 
+    #     n_estimators=params['model']['gradient_boosting']['n_estimators'], 
+    #     random_state=params['model']['gradient_boosting']['random_state'], 
+    #     max_depth=params['model']['gradient_boosting']['max_depth']
+    # )
     
     # Evaluate model
-    # accuracy = evaluate_model(model, X_test, y_test)
+    accuracy = evaluate_model(model, X_test, y_test)
     
     # Save the model
-    # logger.info(f'Saving model to {output_filepath}')
-    # Path(output_filepath).parent.mkdir(parents=True, exist_ok=True)
-    # joblib.dump(model, output_filepath)
+    logger.info(f'Saving model to {output_filepath}')
+    Path(output_filepath).parent.mkdir(parents=True, exist_ok=True)
+    joblib.dump(model, output_filepath)
 
-    logger.info(f'Saving model to {output_filepath2}')
-    Path(output_filepath2).parent.mkdir(parents=True, exist_ok=True)
-    joblib.dump(model_2, output_filepath2)
+    # logger.info(f'Saving model to {output_filepath2}')
+    # Path(output_filepath2).parent.mkdir(parents=True, exist_ok=True)
+    # joblib.dump(model_2, output_filepath2)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
